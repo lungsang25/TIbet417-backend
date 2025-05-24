500a849c628bc5740c3854264d928b88110d5608
Lungsangg <lungsang@esukhia.org>
2025-05-24 19:58:22 +0530

    initial_commit

---
+++ config/mongodb.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log("DB Connected");
+        });
+
+        mongoose.connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+        });
+
+        await mongoose.connect(`${process.env.MONGODB_URI}e-commerce`, {
+            retryWrites: true,
+            w: 'majority'
+        });
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+        throw error;
+    }
+};
+
+export default connectDB;
